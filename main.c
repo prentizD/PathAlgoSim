89799f63c31a5e1c1001cafbaff75cb1213d24aa
prentizD <danila_holstein@yahoo.de>
2022-05-11 00:01:34 +0200

    Buttons and Queue

--- main.c
+++ main.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdbool.h>
+#include <inttypes.h>
 
-#define SCREENWIDTH 800
-#define SCREENHEIGHT 600
-#define ROWS 50
-#define COLUMS 80
+#define SCREENWIDTH 1000
+#define SCREENHEIGHT 800
+#define ROWS 60
+#define COLUMS 60
 #define NODEWIDTH 10
 #define NODEHEIGHT 10
+#define TARGET_FPS 60
+#define START_X 10
+#define START_Y 10
+#define PATHDRAWSPEED 10       // TARGET_FPS * PATHDRAWSPEED = drawn tiles (that are in a queue) per seconds
+#define WEIGHTS 1
 
-typedef struct
+typedef struct tile_position_t
 {
     int row;
     int column;
 } tile_position_t;
 
-typedef struct node{
-    tile_position_t tile_position;
-    struct node* next;
-} node_t;
-
-typedef struct
+typedef struct tile_t
 {
     tile_position_t tile_position;
     Rectangle rectangle;
@@ -35,51 +35,185 @@ typedef struct
     tile_position_t predecessor;
 } tile_t;
 
-node_t *algorythm_path;
-
-int isEmpty() {
-    if (algorythm_path == NULL) 
-        return 1;
-    else
-        return 0;
-}
-
-void push_node(tile_position_t num)
+typedef struct node_t
 {
-    struct node *temp;
-    temp =(struct node *)malloc(1*sizeof(struct node));
-    temp->tile_position = num;
-     
-    if (algorythm_path == NULL) 
+    tile_position_t tile_position;
+    struct node_t* next;
+} node_t;
+
+typedef struct queue_t
+{
+    int count;
+    node_t *front;
+    node_t *rear;
+} queue_t;
+
+static queue_t *algorythm_steps;
+static tile_position_t draw_position;
+static tile_t tiles[ROWS][COLUMS];
+static bool queue_is_empty;
+
+
+bool is_queue_empty(queue_t *q);
+void init_queue(queue_t *q);
+void enqueue(queue_t *q, tile_position_t pos);
+tile_position_t dequeue(queue_t *q);
+
+void draw_tiles(void);
+void init_tiles(void);
+void de_init_tiles(void);
+void set_tile_path(void);
+
+void dijkstra(tile_position_t startnode);
+
+int main(void)
+{
+    InitWindow(SCREENWIDTH, SCREENHEIGHT, "Test");
+    SetTargetFPS(TARGET_FPS);                   // Set our game to run at 60 frames-per-second
+    init_tiles();
+    algorythm_steps = malloc(sizeof(queue_t));
+    init_queue(algorythm_steps);
+    tile_position_t start_node = {START_X, START_Y};
+    //dijkstra(start_node);
+
+    Rectangle button_reset;
+    button_reset.height = 60;
+    button_reset.width = 120;
+    button_reset.x = SCREENWIDTH - 300;
+    button_reset.y = SCREENHEIGHT - 80;
+
+    Rectangle button_dijkstra;
+    button_dijkstra.height = 60;
+    button_dijkstra.width = 120;
+    button_dijkstra.x = SCREENWIDTH - 600;
+    button_dijkstra.y = SCREENHEIGHT - 80;
+
+    double time = 0;
+    char time_buffer[50];
+    double timeout = 0;
+
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {  
+        // loop logic
+        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            Vector2 pos = GetMousePosition();
+            uint8_t rowIndex = (uint8_t) (pos.y/NODEHEIGHT);
+            uint8_t colIndex = (uint8_t) (pos.x/NODEWIDTH);
+            if((rowIndex < ROWS) && (colIndex < COLUMS))
+            {
+                tiles[rowIndex][colIndex].color = RED;
+                tiles[rowIndex][colIndex].weight = 1000;
+                if(rowIndex > 0 && colIndex >0)
+                {
+                    tiles[rowIndex+1][colIndex].color = RED;
+                    tiles[rowIndex+1][colIndex].weight = 1000;               
+                    tiles[rowIndex][colIndex+1].color = RED;
+                    tiles[rowIndex][colIndex+1].weight = 1000;
+                }
+            }
+            if(GetTime()-timeout >= 0.5f)
+            {
+                timeout = GetTime();
+                if(CheckCollisionPointRec(pos, button_dijkstra))
+                { 
+                    
+                    time = GetTime();
+                    dijkstra(start_node);
+                    time = GetTime() - time;
+                    sprintf(time_buffer, "%.10f [ms]", time * 1000);
+                }
+                if(CheckCollisionPointRec(pos, button_reset))
+                {
+                    init_queue(algorythm_steps);
+                    de_init_tiles();
+                    init_tiles();
+                }
+            }
+        }
+        
+        if(!queue_is_empty)
+        {
+            set_tile_path();
+        }
+        // drawing
+        BeginDrawing();
+        {
+            ClearBackground(WHITE);
+            draw_tiles();
+            DrawRectangleRec(tiles[START_X][START_Y].rectangle, YELLOW);
+            DrawText(time_buffer, button_dijkstra.x, button_dijkstra.y - 40, 24, BLACK);
+            DrawRectangleRec(button_reset, RED);
+            DrawRectangleRec(button_dijkstra, YELLOW);
+            DrawFPS(SCREENWIDTH -80, SCREENHEIGHT - 80);
+        }
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();        // Close window and OpenGL context
+    de_init_tiles();
+    
+    return 0;
+}
+
+void init_queue(queue_t *q)
+{
+    q->count = 0;
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+bool is_queue_empty(queue_t *q)
+{
+    if(q->rear == NULL)
     {
-        algorythm_path = temp;
-        algorythm_path->next = NULL;
+        queue_is_empty = true;
+        return true;
     }
     else
     {
-        temp->next = algorythm_path;
-        algorythm_path = temp;
+        queue_is_empty = false;
+        return false;
     }
 }
 
-tile_position_t pop_node() 
+void enqueue(queue_t *q, tile_position_t pos)
 {
-    struct node *temp;
-    if (isEmpty(algorythm_path)) 
+    node_t *tmp;
+    tmp = malloc(sizeof(node_t));
+    tmp->tile_position = pos;
+    tmp->next = NULL;
+    if(is_queue_empty(q) == false)
     {
-        return (tile_position_t){0};
-    } 
-    else 
-    {
-        temp = algorythm_path;
-        algorythm_path = algorythm_path->next;
-        free(temp); 
-        return algorythm_path->tile_position;
+        q->rear->next = tmp;
+        q->rear = tmp;
     }
+    else
+    {
+        q->front = q->rear = tmp;
+    }
+    q->count++;
 }
 
-
-tile_t tiles[ROWS][COLUMS] = {0}; // init all variables to 0
+tile_position_t dequeue(queue_t *q)
+{
+    node_t *tmp;
+    if(q->count != 0)
+    {
+        tile_position_t n = q->front->tile_position;
+        tmp = q->front;
+        q->front = q->front->next;
+        q->count--;
+        free(tmp);
+        return n;
+    }
+    else
+    {
+        return (tile_position_t){0,0};
+    }
+}
 
 void draw_tiles(void)
 {
@@ -114,11 +248,7 @@ void init_tiles(void)
                 tiles[row][column].color = SKYBLUE;
             }
 
-            tiles[row][column].weight = GetRandomValue(0,1);
-            if(row %2 == 0)
-            {
-                tiles[row][column].weight = GetRandomValue(0,2000);
-            }
+            tiles[row][column].weight = GetRandomValue(1, WEIGHTS);
             tiles[row][column].finalized = false;
 
             // allocate space for 4 neighbours
@@ -151,68 +281,18 @@ void de_init_tiles(void)
     }
 }
 
-tile_position_t draw_position;
-
-#define PATHSPEED 10
 void set_tile_path(void)
 {
-    for (size_t i = 0; i < PATHSPEED; i++)
+    for (size_t i = 0; i < PATHDRAWSPEED; i++)
     {
-        if((algorythm_path->next != NULL))
+        if(!queue_is_empty)
         {
-            draw_position = pop_node();
-            tiles[draw_position.row][draw_position.column].color = BLACK;
+            draw_position = dequeue(algorythm_steps);
+            tiles[draw_position.row][draw_position.column].color = GREEN;
         }   
     }
 }
 
-
-void dijkstra(tile_position_t startnode);
-
-int main(void)
-{
-    InitWindow(SCREENWIDTH, SCREENHEIGHT, "Test");
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    init_tiles();
-    tile_position_t start_node = {ROWS/2,COLUMS/2};
-    dijkstra(start_node);
-    
-    float elapsed_frametime = 0.0f;
-
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {  
-
-        // loop logic
-        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-        {
-            Vector2 pos = GetMousePosition();
-            int rowIndex = pos.y/NODEHEIGHT;
-            int colIndex = pos.x/NODEWIDTH;
-            if((rowIndex < ROWS) && (colIndex < COLUMS))
-            {
-                tiles[rowIndex][colIndex].color = RED;
-            }
-            set_tile_path();
-        }
-
-
-        // drawing
-        BeginDrawing();
-        {
-            ClearBackground(WHITE);
-            draw_tiles();
-            DrawFPS(SCREENWIDTH -80, SCREENHEIGHT - 80);
-        }
-        EndDrawing();
-    }
-
-    // De-Initialization
-    CloseWindow();        // Close window and OpenGL context
-    de_init_tiles();
-    
-    return 0;
-}
 
 void dijkstra_update_distance(tile_position_t current_u)
 {
@@ -255,13 +335,11 @@ void dijkstra(tile_position_t startnode)
             }
         }
     }
-    
+    queue_is_empty = false;
     bool finished = false;
     int min_dist = INT_MAX;
     int tmp_min_dist = INT_MAX;
     tile_position_t current_u;
-    tile_position_t allInOrder[ROWS*COLUMS];
-    int i = 0;
     while (!finished)
     {
         min_dist = INT_MAX;
@@ -294,16 +372,6 @@ void dijkstra(tile_position_t startnode)
                 }
             }
         }
-
-        allInOrder[i] = current_u;
-        i++;
-        
-        //printf("Row: %d Col: %d Reach_Cost: %d \n", current_u.row, current_u.column, tiles[current_u.row][current_u.column].reach_cost);
+        enqueue(algorythm_steps, current_u);
     }
-
-    for (size_t j = ROWS*COLUMS; j > 0; j--)
-    {
-        push_node(allInOrder[j-1]);
-    }
-        
 }
